11aada6d68d5b7876f7f41c5ada2c3a3deb15238
Stephen L Arnold <sarnold@vctlabs.com>
2025-08-31 12:12:01 -0700

    chg: dev: update github workflows and tox cmds, add conftest and marks
    Signed-off-by: Stephen L Arnold <sarnold@vctlabs.com>

--- tests/test_filemap.py
+++ tests/test_filemap.py
@@ -30,7 +30,10 @@ import random
 import itertools
 import tests.helpers
 from itertools import zip_longest
+
 from bmaptool import Filemap
+
+import pytest
 
 # This is a work-around for Centos 6
 try:
@@ -143,7 +146,8 @@ class TestFilemap(unittest.TestCase):
     The test class for this unit tests. Basically executes the '_do_test()'
     function for different sparse files.
     """
-
+    @pytest.mark.linux
+    @pytest.mark.slow
     def test(self):  # pylint: disable=R0201
         """
         The test entry point. Executes the '_do_test()' function for files of
